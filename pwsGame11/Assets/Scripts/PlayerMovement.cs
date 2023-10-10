07b5829f9ad01c9dea46debc259b7aba5b0d2e45
pepijn39 <pepijn.snelders99@gmail.com>
2023-10-10 17:41:56 +0200

    Player character contorller
    t ziet er allemaal heel goofy uit maar je kan lopen springen en crouchen

--- pwsGame11/Assets/Scripts/PlayerMovement.cs
+++ pwsGame11/Assets/Scripts/PlayerMovement.cs
@@ -3,16 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{  
+    [SerializeField] CharacterController2D controller;
+    [SerializeField] float runSpeed = 40f;
+    
+
+    bool jump = false;
+    float horizontalMove = 0f;
+    bool crouch = false;
+
+    void Update()
     {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        
+
+    if (Input.GetButtonDown("Jump"))
+    {
+        jump = true;
         
     }
 
-    // Update is called once per frame
-    void Update()
+    if (Input.GetButtonDown("Crouch"))
     {
-        
+        crouch = true;
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+    else if (Input.GetButtonUp("Crouch"))
+    {
+        crouch = false;
+        GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+
+
+
+    }
+
+
+   
+
+    private void FixedUpdate()
+    {
+    //Move our character
+    controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+    jump = false;
     }
 }
